55b6de2012e6db2ca476ac70d1f4ddb2ad56f399
RubenAdarme <radarmef@gmail.com>
2025-10-07 20:34:09 -0400

    Subiendo Practica Punteros

--- programación-2-Adarme/Programa_Clinico.cpp
+++ programación-2-Adarme/Programa_Clinico.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;  
 
 main (){
-cout<<"Hola Mundo"<<endl;
-return 0;   
+
+int numero, *dir_numero;
+
+cout<<"Digite un numero: ";
+cin>>numero;
+
+dir_numero=&numero;
+
+if(*dir_numero%2==0){
+    cout<< "El numero es par"<<*dir_numero<<endl;
+    cout<< "La direccion del numero par es: "<<dir_numero<<endl;
+}else{
+    cout << "El numero es impar "<<*dir_numero<<endl;
+    cout << "la dirección del numero impar es: "<<dir_numero<<endl;
+}
+
 }
